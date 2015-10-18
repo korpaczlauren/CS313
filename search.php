2fdedee8070940cee18b6630d376ac687b30a9ff
korpaczlauren <lauren.korpacz@gmail.com>
2015-10-17 21:19:42 -0600

    Create search.php

---
+++ search.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 0.2b
+ */
+
+//
+// Database `example_database`
+//
+
+// `example_database`.`example_table`
+$example_table = array(
+);
