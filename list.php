614cd1cce35a7b539b9ff1cb650ac47c178b9b64
korpaczlauren <lauren.korpacz@gmail.com>
2015-10-17 21:08:12 -0600

    Create list.php

---
+++ list.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$my_array = array("Shirts","Shoes","Skirts");
+
+list($a, $b, $c) = $my_array;
+echo "There will be several clothes in my store, a $a, a $b and a $c.";
+?>
+
+</body>
+</html>
