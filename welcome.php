eae2b062b2011fb787e83dea0a65172f0bf41cb7
korpaczlauren <lauren.korpacz@gmail.com>
2015-10-25 16:30:03 -0600

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["personsid"]; ?><br>
+Your last name is: <?php echo $_GET["lastname"]; ?><br>
+Your first name is: <?php echo $_GET["firstname"]; ?><br>
+Your address: <?php echo $_GET["address"]; ?><br>
+Your city is: <?php echo $_GET["city"]; ?><br>
+
+
+</body>
+</html>
+
