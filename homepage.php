65cfdc067d8566583292d15f68c08c29b6ae147a
korpaczlauren <lauren.korpacz@gmail.com>
2015-10-25 16:30:44 -0600

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html> 
+<body>
+
+<form action="welcome_get.php" method="get">
+PersonsID: <input type="text" name="personsID"><br>
+Last Name: <input type="text" name="lastname"><br>
+First Name: <input type="text" name="firstname"><br>
+Address: <input type="text" name="address"><br>
+City: <input type="text" name="city"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
